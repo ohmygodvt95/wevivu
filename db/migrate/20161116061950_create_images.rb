7c8d39f624a0dc9962001696c0f7b9933910b134
ThienLV <ohmygodvt95@gmail.com>
2016-11-24 13:46:11 +0700

    add attr

--- db/migrate/20161116061950_create_images.rb
+++ db/migrate/20161116061950_create_images.rb
@@ -5,6 +5,9 @@ class CreateImages < ActiveRecord::Migration
       t.integer :post_id
       t.string :title
       t.string :src
+      t.integer :active, default: 0
+      #0 - post image, 1 - avatar, 2 - cover
+      t.integer :use_for, default: 0
       t.timestamps null: false
     end
   end
