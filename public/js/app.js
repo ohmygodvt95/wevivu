a6311c4a757bf83e28ddbbe06f6bfc7a2e6eda1c
ThienLV <ohmygodvt95@gmail.com>
2016-11-27 23:34:49 +0700

    add lib

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("wevivu", ['ngMaterial', 'ngRoute', 'wu.masonry', 'ngFileUpload']);
+var app = angular.module("wevivu", ['ngMaterial', 'ngRoute', 'wu.masonry', 'ngFileUpload', 'infinite-scroll', 'ngToast']);
 app.version = '/api/v1/';
 app.user_id = $('meta[userid]').attr('userid');
 
@@ -7,6 +7,10 @@ app.config(function ($routeProvider) {
         .when("/", {
             templateUrl: '/pages/home/home.tmpl.html',
             controller: 'HomeController'
+        })
+        .when("/wall", {
+            templateUrl: '/pages/wall/wall.tmpl.html',
+            controller: 'WallController'
         })
         .otherwise({
             redirectTo: '/'
