6195e4412a50cf0007fdd2906be095177bfba7c1
ThienLV <ohmygodvt95@gmail.com>
2016-12-08 17:12:11 +0700

    add cover controller

---
+++ app/controllers/cover_controller.rb
@@ -0,0 +1,13 @@
+class CoverController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def update
+      user = User.find current_user.id
+      if user.update cover: params[:file]
+        render json: {status: "success", data: user}, status: 200
+      else
+        render json: {status: "failure", data: user}, status: 501
+      end
+
+  end
+end
