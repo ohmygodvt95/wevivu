e77820f560f53cfad4732b2f503ef67c5b42e2ed
ThienLV <ohmygodvt95@gmail.com>
2016-12-12 03:03:22 +0700

    add route

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,27 @@ var app = angular.module("wevivu", ['ngMaterial', 'ngRoute', 'wu.masonry', 'ngFi
 app.version = '/api/v1/';
 app.user_id = $('meta[userid]').attr('userid');
 
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $mdDateLocaleProvider) {
+    $mdDateLocaleProvider
+        .formatDate = function(date) {
+            return date ? moment(date).format('YYYY-MM-DD') : '';
+    };
+
     $routeProvider
         .when("/", {
             templateUrl: '/pages/home/home.tmpl.html',
             controller: 'HomeController'
         })
-        .when("/wall", {
+        .when("/wall/:user_id", {
             templateUrl: '/pages/wall/wall.tmpl.html',
             controller: 'WallController'
         })
-        .when("/user/:email", {
-            templateUrl: '/pages/wall/user.tmpl.html'
+        .when("/wall/:user_id/profile", {
+            templateUrl: '/pages/wall/profile.tmpl.html',
+            controller: 'ProfileController'
+        })
+        .when('/error', {
+            templateUrl: '/pages/404.tmpl.html',
         })
         .otherwise({
             redirectTo: '/'
