a9fa2a59fcf0813a2ca67bb3059c692613833069
ThienLV <ohmygodvt95@gmail.com>
2016-12-12 03:05:48 +0700

    add mail setting

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'runsystem.net',
+    :password => 'thien@123',
+    :domain => 'wevivu.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
