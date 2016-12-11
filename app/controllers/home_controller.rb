14796b3170542a694cf8690d214a40c177373956
ThienLV <ohmygodvt95@gmail.com>
2016-12-12 03:06:34 +0700

    add request for rate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  MAX = 99999999999999
+  MAX = 9999999999999999
 
   def index
   end
@@ -8,22 +8,57 @@ class HomeController < ApplicationController
   def get_posts
     case params[:type]
       when 'me'
-        realtime_page_next_by_me Integer(params[:after]), Integer(params[:limit])
+        realtime_page_next_by_me Integer(params[:user_id]), Integer(params[:after]), Integer(params[:limit])
       when 'rates'
+        realtime_page_next_by_rate Integer(params[:after]), Integer(params[:limit])
+      when 'search'
+        realtime_page_next_by_search Integer(params[:after]), Integer(params[:limit]), params[:query]
       when 'bookmarks'
-        realtime_page_next_by_bookmark Integer(params[:after]), Integer(params[:limit]), params[:query]
+        realtime_page_next_by_bookmark Integer(params[:user_id]), Integer(params[:after]), Integer(params[:limit]), params[:query]
       else
         realtime_page_next_by_time Integer(params[:after]), Integer(params[:limit]), params[:query]
     end
   end
 
+
   private
 
-  def realtime_page_next_by_bookmark(after, limit, query)
+  def realtime_page_next_by_rate(after, limit)
+    after = MAX if after <= 0
+    list = Rate.group(:post_id).order("count_all desc").includes(:post).limit(50).count
+    posts = []
+    list.each do |item|
+      posts.append Post.find item[0]
+    end
+    render json: {
+        status: 'success',
+        data: posts.as_json(
+            include: [
+                {
+                    user:
+                        {
+                            only: [:id, :name, :avatar]
+                        }
+                },
+                :location,
+                {
+                    images: {
+                        only: [:id, :src]
+                    }
+                },
+                :rates]),
+        total: posts.count,
+        limit: limit,
+        after: MAX,
+        before: 0
+    }, status: :ok
+  end
+
+  def realtime_page_next_by_bookmark(user_id, after, limit, query)
     after = MAX if after <= 0
 
     if query.length == 0
-      bookmarks = Bookmark.where('user_id = ? AND id < ?', current_user.id, after).order(id: :desc).limit limit
+      bookmarks = Bookmark.where('user_id = ? AND id < ?', user_id, after).order(id: :desc).limit limit
       posts = []
       bookmarks.each do |item|
         posts.append item.post unless item.post.nil?
@@ -60,9 +95,39 @@ class HomeController < ApplicationController
     }, status: :ok
   end
 
-  def realtime_page_next_by_me(after, limit)
+  def realtime_page_next_by_me(user_id, after, limit)
     after = MAX if after <= 0
-    posts = Post.where("id < ? AND user_id = ?", after, current_user.id).order(id: :desc).limit limit
+    posts = Post.where("id < ? AND user_id = ?", after, user_id).order(id: :desc).limit limit
+
+    render json: {
+        status: 'success',
+        data: posts.as_json(
+            include: [
+                {
+                    user:
+                        {
+                            only: [:id, :name, :avatar]
+                        }
+                },
+                :location,
+                {
+                    images: {
+                        only: [:id, :src]
+                    }
+                },
+                :rates]),
+        total: posts.count,
+        limit: limit,
+        after: posts.count == 0 ? after : posts.last.id,
+        before: after == MAX ? (posts.count == 0 ? 0 : posts.last.id) : 0
+    }, status: :ok
+  end
+
+  def realtime_page_next_by_search(after, limit, query)
+    after = MAX if after <= 0
+
+    q = Post.ransack body_cont: query, location_name_cont: query, user_name_cont: query, user_email_cont: query, m: 'or'
+    posts = q.result.where("posts.id < ?", after).limit limit
 
     render json: {
         status: 'success',
